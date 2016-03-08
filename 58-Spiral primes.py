9086a9794acf53e5bdbb82301f462b53a0cc4c5f
Katya <kolikova@gmail.com>
2016-03-08 10:15:47 -0800

    problem 58 - classic checking for primality

---
+++ 58-Spiral primes.py
@@ -0,0 +1,30 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num%2 == 0 or num%3 == 0:
+        return False
+    i = 5
+    while i*i <= num:
+        if num%i == 0 or num%(i+2) == 0:
+            return False
+        i += 6
+    return True
+
+l = 3
+nprimes = 3
+ntot = 5
+
+while True:
+    dia = []
+    dia1 = l*l + l + 1
+    l += 1
+    nprimes += sum(is_prime(dia1 + i*l) for i in range(3))
+    l += 1
+    ntot += 4
+
+    if nprimes/ntot < 0.1:
+        print(l)
+        break
+
