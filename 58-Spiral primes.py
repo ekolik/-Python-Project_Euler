b920a7ffc5020584cb119f1480af30b87708cb01
Katya <kolikova@gmail.com>
2016-03-08 10:57:14 -0800

    problem 58 - Miller-Rabin primality test + execution time

--- 58-Spiral primes.py
+++ 58-Spiral primes.py
@@ -1,19 +1,32 @@
 import time
+import random
+import sys
 start_time = time.time()
 
-def is_prime(num):
-    if num <= 1:
+
+def is_prob_prime(n, k=7):
+    if n < 6:  # assume n >= 0 always
+        return [False, False, True, True, False, True][n]
+    elif n & 1 == 0:  # faster than n%2
         return False
-    elif num <= 3:
+    else:
+        s, d = 0, n-1
+        while d & 1 == 0:
+            s, d = s+1, d >> 1  # equal to d/2
+        for a in random.sample(range(2, min(n-2, sys.maxsize)), min(n-4, k)):
+            x = pow(a, d, n)
+            if x != 1 and x+1 != n:
+                for r in range(1, s):
+                    x = pow(x, 2, n)
+                    if x == 1:
+                        return False
+                    elif x == n-1:
+                        a = 0  # so we know the loop didn't come to end
+                        break
+                if a:
+                    return False
         return True
-    elif num%2 == 0 or num%3 == 0:
-        return False
-    i = 5
-    while i*i <= num:
-        if num%i == 0 or num%(i+2) == 0:
-            return False
-        i += 6
-    return True
+
 
 l = 3
 nprimes = 3
@@ -23,7 +36,7 @@ while True:
     dia = []
     dia1 = l*l + l + 1
     l += 1
-    nprimes += sum(is_prime(dia1 + i*l) for i in range(3))
+    nprimes += sum(is_prob_prime(dia1 + i*l) for i in range(3))
     l += 1
     ntot += 4
 
