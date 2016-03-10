85745e65f214f79c3c6edde82b4432378e6cbfb3
Katya <kolikova@gmail.com>
2016-03-10 15:18:35 -0800

    problem 61 - final

--- 61-Cyclical figurate numbers.py
+++ 61-Cyclical figurate numbers.py
@@ -23,25 +23,12 @@ def formulae(m):
 
 
 def cycle(first, second):
-    heads = set()
-    tails = set()
+    # rewrites 'first' and 'second' so that they are cyclical 'first->second'
+    heads = {str(i)[:2] for i in second}
+    tails = {str(i)[2:] for i in first}
 
-    for i in second:
-        heads |= {str(i)[:2]}
-    for i in first:
-        tails |= {str(i)[2:]}
-
-    # print(heads.intersection(tails))
-
-    first_new = []
-    for i in first:
-        if str(i)[2:] in heads.intersection(tails):
-            first_new.append(i)
-
-    second_new = []
-    for i in second:
-        if str(i)[:2] in heads.intersection(tails):
-            second_new.append(i)
+    first_new = [i for i in first if str(i)[2:] in heads.intersection(tails)]
+    second_new = [i for i in second if str(i)[:2] in heads.intersection(tails)]
 
     return [first_new, second_new]
 
@@ -50,6 +37,7 @@ all_poss = [formulae(i) for i in range(3, 9)]
 perms = list(permutations(range(6)))
 
 for perm in perms:
+    # first, initializing step in creating 6 arrays, each cycles to the next
     first_num, second_num = cycle(all_poss[perm[0]], all_poss[perm[1]])
     second_num, third_num = cycle(second_num, all_poss[perm[2]])
     third_num, forth_num = cycle(third_num, all_poss[perm[3]])
@@ -57,7 +45,7 @@ for perm in perms:
     fifth_num, sixth_num = cycle(fifth_num, all_poss[perm[5]])
     sixth_num, first_num = cycle(sixth_num, first_num)
 
-    # narrow down found arrays
+    # continue cycling found arrays to eliminate non-six-length cycles
     while first_num and second_num and third_num and forth_num and fifth_num and sixth_num:
         first_num, second_num = cycle(first_num, second_num)
         second_num, third_num = cycle(second_num, third_num)
