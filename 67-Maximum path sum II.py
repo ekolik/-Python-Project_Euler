39b0207ca4e8e1948017339046ecf879550ce72a
Katya <kolikova@gmail.com>
2016-03-17 10:50:51 -0700

    problem 67 - more compact way to read the triangle

--- 67-Maximum path sum II.py
+++ 67-Maximum path sum II.py
@@ -1,16 +1,8 @@
-# the code is identical to the one for the problem 18
+# the algorithm is identical to the one for the problem 18
 
-with open("67-input.txt") as f:
-    content = f.readlines()
+tria = [list(map(int, l.split())) for l in open("67-input.txt").readlines()]
 
-tria = list()
-
-for i in range(len(content)):
-    tria.append(content[i].split())
-    for j in range(len(tria[i])):
-        tria[i][j] = int(tria[i][j])
-
-for i in range(len(content)-2, -1, -1):
+for i in range(len(tria)-2, -1, -1):
     for j in range(len(tria[i])):
         tria[i][j] += max(tria[i+1][j], tria[i+1][j+1])
 
