1ef15d6fc61bc649fa8d86b722660ce75770a3b2
Katya <kolikova@gmail.com>
2016-03-17 10:44:00 -0700

    problem 67

---
+++ 67-Maximum path sum II.py
@@ -0,0 +1,17 @@
+# the code is identical to the one for the problem 18
+
+with open("67-input.txt") as f:
+    content = f.readlines()
+
+tria = list()
+
+for i in range(len(content)):
+    tria.append(content[i].split())
+    for j in range(len(tria[i])):
+        tria[i][j] = int(tria[i][j])
+
+for i in range(len(content)-2, -1, -1):
+    for j in range(len(tria[i])):
+        tria[i][j] += max(tria[i+1][j], tria[i+1][j+1])
+
+print(tria[0][0])
